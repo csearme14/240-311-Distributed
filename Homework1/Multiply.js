06663172e2aef71bfd180e7d1ee38f89f059f9f0
chalisa sinban <71112083+csearme14@users.noreply.github.com>
2022-01-10 11:46:31 +0700

    homework1
    by 6135512020

---
+++ Homework1/Multiply.js
@@ -0,0 +1,9 @@
+//สูตรคูณ
+let n=5;
+    var sum;
+    var i;
+    n=parseInt(n);
+for(i=0;i<13;i++)
+{
+        console.log(sum = n + "X" +i+ "="+(n*i))
+}
---
+++ Homework1/Triangle_area.js
@@ -0,0 +1,7 @@
+//คำนวณพื้นที่สามเหลี่ยม
+let high = 4;
+let base = 15;
+var sum;
+    high=parseInt(high);
+    base=parseInt(base);
+    console.log(sum = "Triangle area : "+  "1/2" + "*" + high + "*" + base + "="+(high*base*0.5))
